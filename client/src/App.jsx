5e1fb97c23f52e9afe027240c8984c6a41915a35
Esmira11 <ismayilova788@gmail.com>
2023-08-01 22:22:19 +0400

    login page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import AboutPage from './pages/AboutPage'
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 
 function App() {
@@ -21,14 +22,15 @@ function App() {
     <>
 
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
           <Routes>
             <Route path='/' element={<HomePage/>}/>
             <Route path='/about' element={<AboutPage/>}/>
             <Route path='*' element={<NotFoundPage/>}/>
+            <Route path='/login' element={<LoginPage/>}/>
             
           </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
 
       {/* <Header/> */}
---
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./loginPage.scss";
+// import usePasswordToogle from "./usePasswordToogle";
+import { Icon } from 'react-icons-kit'
+import {eyeOff} from 'react-icons-kit/feather/eyeOff'
+import {eye} from 'react-icons-kit/feather/eye'
+
+
+
+function LoginPage() {
+  // const [PasswordInputType, ToogleIcon] = usePasswordToogle();
+
+  const [type,setType] = useState('password');
+  const [icon,setIcon] = useState(eyeOff)
+
+  const handleToogle = () =>{
+    if(type === 'password'){
+      setIcon(eye);
+      setType('text');
+    }
+    else{
+      setIcon(eyeOff);
+      setType('password')
+    }
+  }
+
+  return (
+    <div className="login">
+      <div className="login-container">
+        <div className="login-page-title">
+          <h1 className="login-title">Good to see you again</h1>
+        </div>
+        <div className="login-page-bottom">
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" placeholder="name@example.com" />
+            </div>
+
+            <div className="form-group ">
+              <label htmlFor="password">Password</label>
+              <input type={type} placeholder="password" />
+              <span onClick={handleToogle} className="password-toogle-icon"><Icon size={20} icon={icon} /></span>
+            </div>
+
+            <div className="btn">
+              <button className="submit-btn" type="submit">
+                Sign in
+              </button>
+            </div>
+
+            <div className="links">
+              <div className="link">
+                <a href="#">Don't have an account?</a>
+              </div>
+              <div className="link">
+                <a href="#">Forgot password?</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
