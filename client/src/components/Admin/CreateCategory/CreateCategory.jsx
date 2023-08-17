98e98c983832fde840a70b5bd3e3fbbd10c63f6b
Esmira11 <ismayilova788@gmail.com>
2023-08-18 00:31:11 +0400

    create category full completed

--- client/src/components/Admin/CreateCategory/CreateCategory.jsx
+++ client/src/components/Admin/CreateCategory/CreateCategory.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import toast,{ Toaster }  from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import "./createcategory.scss";
 import Table from "@mui/material/Table";
@@ -9,10 +9,25 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 function CreateCategory() {
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+  const [selected, setSelected] = useState(null)
+  const [updatedName, setUpdatedName] = useState("")
+  const [selectedDelete, setSelectedDelete] = useState(0)
+
+  // const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // const handleOpenEdit = () => setOpenEdit(true);
+  const handleCloseEdit = () => setOpenEdit(false);
 
   const getAllCategory = async () => {
     try {
@@ -43,6 +58,7 @@ function CreateCategory() {
       );
       if (data?.success) {
         toast.success(`${data.category.name} is created!`);
+        getAllCategory();
       } else {
         toast.error(data.message);
       }
@@ -54,6 +70,59 @@ function CreateCategory() {
 
     setName("");
   };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const {data} = await axios.put(`http://localhost:5000/api/category/update-category/${selected._id}`,
+      {name: updatedName})
+      if(data.success){
+        toast.success(`${updatedName} is updated`)
+        setSelected(null)
+        setUpdatedName("")
+        setOpenEdit(false),
+        getAllCategory()
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error('Something went wrong')
+    }
+  }
+
+  const handleDelete = async(id) => {
+    try {
+      const {data} = await axios.delete(`http://localhost:5000/api/category/delete-category/${id}`)
+      if(data.success){
+        toast.success(`Category deleted`);
+        setSelectedDelete(0);
+        getAllCategory()
+        handleClose()
+      }
+      else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error('Something went wrong')
+    }
+  }
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    borderRadius:'15px',
+    backgroundColor:' #fffaf5',
+    color:'#2f4f4f',
+    // bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    border:'none'
+  };
+
 
   return (
     <>
@@ -131,22 +200,73 @@ function CreateCategory() {
                     {row.name}
                   </TableCell>
                   <TableCell component="th" scope="row">
-                  <button
-                      className="btn btn-primary"
-                    >
+                    <button onClick={() => {
+                      setOpenEdit(true) ; 
+                      setUpdatedName(row.name);
+                      setSelected(row)
+                      }} className="btn btn-primary">
                       Edit
-                  </button>
+                    </button>
                   </TableCell>
-                  
+                  <TableCell component="th" scope="row">
+                    <button className="btn btn-primary"
+                     onClick={()=>{setOpen(true); setSelectedDelete(row._id)}}>Delete</button>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-        
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2" style={{textAlign:'center',fontSize:'20px'}}>
+              Are you sure ?
+            </Typography>
+            <div className="btns" style={{display:'flex',justifyContent:'space-between',paddingTop:'20px'}}>
+            <Button className="subbtn" onClick={handleClose} variant="text" style={{background: '#2f4f4f',color: '#fffaf5'}}>Cancel</Button>
+            <Button onClick={()=>{handleDelete(selectedDelete)}} className="subbtn" variant="text" style={{background: '#2f4f4f',color: '#fffaf5'}}>Delete</Button>
+
+            </div>
+           
+            
+          </Box>
+        </Modal>
+
+        <Modal
+          open={openEdit}
+          onClose={handleCloseEdit}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2" style={{textAlign:'center',fontSize:'20px'}}>
+            <form onSubmit={handleUpdate}>
+                <input
+                  style={{width:'80%',padding:'15px',fontSize:'17px', borderRadius:'15px',border:'none',outline:'none',background:'rgba(47, 79, 79, 0.2)'}}
+                  type="text"
+                  // placeholder="Category name"
+                  value={updatedName}
+                  onChange={(e) => setUpdatedName(e.target.value)}
+                />
+            </form>
+            </Typography>
+            <div className="btns" style={{display:'flex',justifyContent:'space-between',paddingTop:'20px'}}>
+            <Button className="subbtn" onClick={handleCloseEdit} variant="text" style={{background: '#2f4f4f',color: '#fffaf5'}}>Cancel</Button>
+            <Button onClick={handleUpdate} className="subbtn" variant="text" style={{background: '#2f4f4f',color: '#fffaf5'}}>Update</Button>
+
+            </div>
+           
+            
+          </Box>
+        </Modal>
       </div>
-      <Toaster position="top-right"/>
-      
+      <Toaster position="top-right" />
     </>
   );
 }
