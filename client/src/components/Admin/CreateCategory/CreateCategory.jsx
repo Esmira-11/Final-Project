aa083ffca83bddbd6a2a76d6db7f56e37ae6b69a
Esmira11 <ismayilova788@gmail.com>
2023-08-17 20:50:01 +0400

    admin dashboard updated

--- client/src/components/Admin/CreateCategory/CreateCategory.jsx
+++ client/src/components/Admin/CreateCategory/CreateCategory.jsx
@@ -1,9 +1,154 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import toast,{ Toaster }  from "react-hot-toast";
+import axios from "axios";
+import "./createcategory.scss";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 function CreateCategory() {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:5000/api/category/all-categories"
+      );
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, [categories]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/api/category/create-category",
+        {
+          name,
+        }
+      );
+      if (data?.success) {
+        toast.success(`${data.category.name} is created!`);
+      } else {
+        toast.error(data.message);
+      }
+      console.log("Form submitted:", { name });
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in input form");
+    }
+
+    setName("");
+  };
+
   return (
-    <div>CreateCategory</div>
-  )
+    <>
+      <div className="create-category">
+        <div className="form-section">
+          <div className="form-title">
+            <h3>Create new category</h3>
+          </div>
+          <div className="form">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Category name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+
+        {/* <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories?.map((item) => (
+              <>
+                <tr
+                  style={{
+                    fontFamily: "'Raleway', sans-serif",
+                    color: "#2f4f4f",
+                  }}
+                >
+                  <td style={{ fontSize: "20px" }} key={item._id}>
+                    {item.name}
+                  </td>
+                  <td style={{ paddingTop: "5px" }}>
+                    <button
+                      style={{
+                        background: "#d5e5e9",
+                        color: "#2f4f4f",
+                        border: "none",
+                      }}
+                      className="btn btn-primary"
+                    >
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </table> */}
+
+        <TableContainer className="table" component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead className="thead">
+              <TableRow>
+                <TableCell className="thcell">Name</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {categories.map((row) => (
+                <TableRow
+                  key={row._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                  <button
+                      className="btn btn-primary"
+                    >
+                      Edit
+                  </button>
+                  </TableCell>
+                  
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        
+      </div>
+      <Toaster position="top-right"/>
+      
+    </>
+  );
 }
 
-export default CreateCategory
+export default CreateCategory;
--- client/src/pages/AdminProfilePage/AdminProfile.jsx
+++ client/src/pages/AdminProfilePage/AdminProfile.jsx
@@ -1,53 +1,86 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import './adminprofile.scss'
-import avatar from "../../assets/images/avatar-3.png"
+import "./adminprofile.scss";
+import avatar from "../../assets/images/avatar-3.png";
 import { useAuth } from "../../context/auth";
-import CreateProduct from "../../components/Admin/CreateProduct/CreateProduct"
+import CreateProduct from "../../components/Admin/CreateProduct/CreateProduct";
 // import { Link } from 'react-router-dom'
-import Detail from '../DetailPage/Detail'
+import Detail from "../DetailPage/Detail";
 import CreateCategory from "../../components/Admin/CreateCategory/CreateCategory";
 import Users from "../../components/Admin/Users/Users";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
+
 function AdminProfile() {
   const [auth, setAuth] = useAuth();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <Layout>
-      {/* <div className="list-group">
-        <a href="#" className="list-group-item list-group-item-action active" aria-current="true">
-          Profile
-        </a>
-        <Link to="/profile/admin/users" className="list-group-item list-group-item-action">
-          Users
-        </Link>
-        <Link to="/profile/admin/create-category" className="list-group-item list-group-item-action">
-          Create Category
-        </Link>
-        <Link to="/profile/admin/create-product" className="list-group-item list-group-item-action">
-          Create Product
-        </Link>
-        <Link to="/profile/admin/create-announcement" className="list-group-item list-group-item-action">
-          Create Announcement
-        </Link>
-        <Link to="/profile/admin/messages" className="list-group-item list-group-item-action">
-          Messages
-        </Link>
-      </div> */}
-
       <div className="admin-profile">
         <div className="admin-profile-container">
-          <Tabs id="tab">
-            <TabList className="tabname">
-              <Tab className="tabtitle">Profile</Tab>
-              <Tab className="tabtitle">Users</Tab>
-              <Tab className="tabtitle">Create Category</Tab>
-              <Tab className="tabtitle">Create Product</Tab>
-              <Tab className="tabtitle">Create Announcement</Tab>
-              <Tab className="tabtitle">Messages</Tab>
-            </TabList>
-
-            <TabPanel className="user-panel">
+          <Box
+            className="box"
+            sx={{ flexGrow: 1, bgcolor: "background.paper", display: "flex" }}
+          >
+            <Tabs
+              className="tabs"
+              orientation="vertical"
+              variant="scrollable"
+              value={value}
+              onChange={handleChange}
+              aria-label="Vertical tabs example"
+              sx={{ borderRight: 1, borderColor: "divider" }}
+            >
+              <Tab label="Profile" {...a11yProps(0)} />
+              <Tab label="Users" {...a11yProps(1)} />
+              <Tab label="Category" {...a11yProps(2)} />
+              <Tab label="Pet Category" {...a11yProps(3)} />
+              <Tab label="Product" {...a11yProps(4)} />
+              <Tab label="Announcement" {...a11yProps(5)} />
+              <Tab label="Messages" {...a11yProps(6)} />
+            </Tabs>
+            <TabPanel className="tabpanel user-panel" value={value} index={0}>
               <div className="user-detail-container">
                 <div className="user-detail" width="100%" height="100%">
                   <div className="user-detail-img">
@@ -64,27 +97,25 @@ function AdminProfile() {
                 </div>
               </div>
             </TabPanel>
-
-            <TabPanel className="user-panel">
+            <TabPanel className="tabpanel" value={value} index={1}>
               <Users/>
             </TabPanel>
-
-            <TabPanel className="user-panel">
+            <TabPanel className="tabpanel" value={value} index={2}>
               <CreateCategory/>
             </TabPanel>
-
-            <TabPanel className="user-panel">
+            <TabPanel className="tabpanel" value={value} index={3}>
+              
+            </TabPanel>
+            <TabPanel className="tabpanel" value={value} index={4}>
               <CreateProduct/>
             </TabPanel>
-
-            <TabPanel className="user-panel">
-              
+            <TabPanel className="tabpanel" value={value} index={5}>
+              Announcement
             </TabPanel>
-
-            <TabPanel className="user-panel">
-              
+            <TabPanel className="tabpanel" value={value} index={6}>
+              Messages
             </TabPanel>
-          </Tabs>
+          </Box>
         </div>
       </div>
     </Layout>
